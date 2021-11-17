7ff014f4ac81cfa9a307c325b2bc19705d341ebd
Andreas Cauê Cabral Magalhães <andreascauecm@gmail.com>
2021-11-16 21:39:21 -0400

    Create API Project

---
+++ Back/src/1VALET.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace _1VALET.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventController : ControllerBase
+    {        
+        public EventController()
+        {
+           
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+          return "value";
+        }
+    }
+}
