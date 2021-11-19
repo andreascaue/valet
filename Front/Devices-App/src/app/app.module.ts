15a5f33d79051cba397e8306a898a4459a7d2952
Andreas Cauê Cabral Magalhães <andreascauecm@gmail.com>
2021-11-19 06:08:01 -0400

    Images and Search

--- Front/Devices-App/src/app/app.module.ts
+++ Front/Devices-App/src/app/app.module.ts
@@ -1,23 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DevicesComponent } from './devices/devices.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
 
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DevicesComponent
+    DevicesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/Devices-App/src/app/devices/devices.component.ts
+++ Front/Devices-App/src/app/devices/devices.component.ts
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DevicesComponent implements OnInit {
 
-  public itensDevices: any;
+  public devices: any = [];
+  public devicesFiltered: any = [];
+
+  widthImage: number = 50;
+  marginImagem: number = 2;
+  showImage: boolean = true;
+  private _filterList: string = '';
 
   constructor(private http: HttpClient) { }
 
@@ -16,11 +22,39 @@ export class DevicesComponent implements OnInit {
     this.getDevices();
   }
 
+  public get filerList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.devicesFiltered = this.filterList ? this.filterDevices(this.filterList) : this.devices;
+  }
+
+  filterDevices(filterby: string): any {
+    filterby = filterby.toLocaleLowerCase();
+    return this.devices.filter(
+      (device: {name: string; details: string; }) => device.name.toLocaleLowerCase().indexOf(filterby) !== -1 ||
+      device.details.toLocaleLowerCase().indexOf(filterby) !== -1
+    )
+  }
+
+  changeImage() {
+    this.showImage = !this.showImage;
+  }
+
+  clearSearch() {
+    this.filterList = '';
+  }
+
   public getDevices(): void {
-     this.http.get('http://localhost:5000/Devices').subscribe(
-       response => this.itensDevices = response,
-       error => console.log(error)
-     );
+    this.http.get('http://localhost:5000/Devices').subscribe(
+      response => {
+        this.devices = response;
+        this.devicesFiltered = this.devices;
+      },
+      error => console.log(error)
+    );
   }
 
 }
